3482a525557a65cd84103d7af102b1363522b649
ElianMelo <elianmelomorais@gmail.com>
2018-12-26 17:43:36 -0200

    Insere multiplos registros utilizando a ordem da tabela

--- consultas.sql
+++ consultas.sql
@@ -31,12 +31,14 @@ CREATE TABLE pessoas (
 DESCRIBE pessoas;
 
 -- Insere valores na tabela pessoas -- 
-INSERT INTO pessoas
-(id, nome, nascimento, sexo, peso, altura, nacionalidade)
-VALUES
--- ('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
--- ('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal'),
-(DEFAULT, 'Creuza', '1920-12-30', 'F', '50.2', '1.65', DEFAULT);
+INSERT INTO pessoas VALUES
+('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
+('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal'),
+(DEFAULT, 'Creuza', '1920-12-30', 'F', '50.2', '1.65', DEFAULT),
+(DEFAULT, 'Adalgiza', '1930-11-02', 'F', '63.2', '1.75', 'Irlanda'),
+(DEFAULT, 'Cláudio', '1975-04-22', 'M', '99.0', '2.15', 'Brasil'),
+(DEFAULT, 'Pedro', '1999-12-03', 'M', '87', '2', DEFAULT),
+(DEFAULT, 'Janaína', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
 
 -- Exibe todos os campos e registros da tabela pessoas -- 
 SELECT * FROM pessoas; 
