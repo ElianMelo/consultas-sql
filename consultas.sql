032b3cd317b95cf10f74b11dab15d70087fb2b07
ElianMelo <elianmelomorais@gmail.com>
2018-12-26 17:30:31 -0200

    Insere dois registros na tabela pessoas

--- consultas.sql
+++ consultas.sql
@@ -4,14 +4,18 @@ Data de criação do arquivo: 26/12/2018
 Objetivo sucinto do arquivo: Demonstrar operações na linguagem SQL
 Referência: https://www.youtube.com/playlist?list=PLHz_AreHm4dkBs-795Dsgvau_ekxg8g1r
 */
+-- Usa o banco de dados cadastro -- 
 USE cadastro;
 
+-- Deleta o banco de dados cadastro --
 DROP DATABASE cadastro; 
 
+-- Cria o banco de dados cadastro --
 CREATE DATABASE cadastro 
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci; 
 
+-- Cria a tabela pessoas --
 CREATE TABLE pessoas (
 	id int NOT NULL AUTO_INCREMENT,
 	nome varchar(30) NOT NULL, 
@@ -23,4 +27,16 @@ CREATE TABLE pessoas (
     PRIMARY KEY(id)
 )DEFAULT CHARSET = utf8;
 
+-- Exibe as informações da tabela pessoas --
 DESCRIBE pessoas;
+
+-- Insere valores na tabela pessoas -- 
+INSERT INTO pessoas
+(nome, nascimento, sexo, peso, altura, nacionalidade)
+VALUES
+('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
+('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal');
+
+-- Exibe todos os campos e registros da tabela pessoas -- 
+SELECT * FROM pessoas; 
+
