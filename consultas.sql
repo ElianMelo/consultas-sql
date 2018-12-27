c96653868e99d6688e67d045634ff1663c5b9ed4
ElianMelo <elianmelomorais@gmail.com>
2018-12-27 15:49:18 -0200

    Adiciona diversos comandos de alteracao e adiciona a tabela cursos

--- consultas.sql
+++ consultas.sql
@@ -16,7 +16,7 @@ DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci; 
 
 -- Cria a tabela pessoas --
-CREATE TABLE pessoas (
+CREATE TABLE pessoas(
 	id int NOT NULL AUTO_INCREMENT,
 	nome varchar(30) NOT NULL, 
     nascimento date,
@@ -29,11 +29,13 @@ CREATE TABLE pessoas (
 
 -- Exibe as informações da tabela pessoas --
 DESCRIBE pessoas;
+DESC pessoas;
+DESC gafanhotos;
 
 -- Insere valores na tabela pessoas -- 
 INSERT INTO pessoas VALUES
-('Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
-('Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal'),
+(DEFAULT, 'Godofredo', '1984-01-02', 'M', '78.5', '1.83', 'Brasil'),
+(DEFAULT, 'Maria', '1999-12-30', 'F', '55.2', '1.65', 'Portugal'),
 (DEFAULT, 'Creuza', '1920-12-30', 'F', '50.2', '1.65', DEFAULT),
 (DEFAULT, 'Adalgiza', '1930-11-02', 'F', '63.2', '1.75', 'Irlanda'),
 (DEFAULT, 'Cláudio', '1975-04-22', 'M', '99.0', '2.15', 'Brasil'),
@@ -41,5 +43,41 @@ INSERT INTO pessoas VALUES
 (DEFAULT, 'Janaína', '1987-11-12', 'F', '75.4', '1.66', 'EUA');
 
 -- Exibe todos os campos e registros da tabela pessoas -- 
-SELECT * FROM pessoas; 
+SELECT * FROM pessoas;
 
+-- Altera a tabela pessoas adicionando a coluna profissao -- 
+ALTER TABLE pessoas
+ADD COLUMN profissao varchar(10); 
+
+-- Altera a tabela pessoas removendo a coluna profissao --
+ALTER TABLE pessoas
+DROP COLUMN profissao;
+
+-- Altera a tabela pessoas adicionando a coluna profissao após da coluna nome --
+ALTER TABLE pessoas
+ADD COLUMN profissao varchar(10) AFTER nome;
+
+-- Altera a tabela pessoas adicionando a coluna codigo na primeira posição --
+ALTER TABLE pessoas
+ADD /* COLUMN */ codigo int FIRST;
+
+-- Altera a tabela pessoas modificando a coluna profissao --
+ALTER TABLE pessoas
+MODIFY COLUMN profissao varchar(20) NOT NULL DEFAULT ''; 
+
+-- Altera a tabela pessoas modificando o nome da coluna profissao --
+ALTER TABLE pessoas
+CHANGE COLUMN profissao prof varchar(20);
+
+-- Altera a tabela pessoas modificando o seu nome para gafanhotos --
+ALTER TABLE pessoas
+RENAME TO gafanhotos;
+
+-- Cria a tabela cursos --
+CREATE TABLE IF NOT EXISTS cursos(
+	nome varchar(30) NOT NULL UNIQUE,
+	descricao text,
+	carga int UNSIGNED,
+    totaulas int UNSIGNED,
+    ano year DEFAULT '2016'
+)DEFAULT CHARSET = utf8;
